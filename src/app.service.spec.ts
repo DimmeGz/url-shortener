844ddd2e91903a267a51c6240b6fa1427d8b7464
Dima Serhiienko <dima.sergienko@gmail.com>
2023-09-02 13:56:26 +0300

    test: add app.service.spec.ts

--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -8,6 +8,29 @@ import { Connection } from 'typeorm';
 import { AuthService } from './auth/auth.service';
 import { JwtModule } from '@nestjs/jwt';
 
+const undefinedFindOne = undefined;
+const createdShortenUrl = {
+  id: 1,
+  full_url: 'example.com',
+  shorten_url: 'As3hI3C',
+  usage_count: 0,
+};
+
+const usersUrls = {
+  owner: {
+    id: 1,
+  },
+  full_url: 'example.com',
+  shorten_url: 'As3hI3C',
+  usage_count: 0,
+  id: 1,
+};
+
+const redisUrl = JSON.stringify({
+  u: 'example.com',
+  a: 1,
+});
+
 describe('AppService', () => {
   let service: AppService;
 
@@ -18,12 +41,20 @@ describe('AppService', () => {
         AppService,
         {
           provide: getRepositoryToken(ShortenUrl),
-          useValue: {},
+          useValue: {
+            findOne: jest.fn().mockReturnValue(undefinedFindOne),
+            find: jest.fn().mockReturnValue([usersUrls]),
+            create: jest.fn().mockReturnValue(createdShortenUrl),
+            save: jest.fn().mockReturnValue(createdShortenUrl),
+          },
         },
 
         {
           provide: REDIS_URLS,
-          useValue: {},
+          useValue: {
+            set: jest.fn(),
+            get: jest.fn().mockReturnValue(redisUrl),
+          },
         },
       ],
     }).compile();
@@ -33,5 +64,20 @@ describe('AppService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('test url create', async () => {
+    const res = await service.createUrl('some_link');
+    expect(res).toEqual(createdShortenUrl);
+  });
+
+  it('test urls get', async () => {
+    const res = await service.getUserUrls(1);
+    expect(res).toEqual([usersUrls]);
+  });
+
+  it('test redirect', async () => {
+    const res = await service.redirect('As3hI3C');
+    expect(res).toEqual({ url: 'https://example.com' });
   });
 });
